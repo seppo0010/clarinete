c4ffe802395dbda9e1b345e67b28208e883c2c61
Sebastian Waisbrot <seppo0010@gmail.com>
2021-06-29 09:23:07 -0300

    Fix date parsing

--- scrapy/clarinetear/spiders/clarin.py
+++ scrapy/clarinetear/spiders/clarin.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 import scrapy
 import lxml
 from lxml.html.clean import Cleaner
 
+cleaner = Cleaner(allow_tags=['p', 'br', 'b', 'a', 'strong', 'i', 'em'])
 class ClarinSpider(scrapy.Spider):
     name = 'clarin'
     allowed_domains = ['clarin.com']
@@ -14,24 +16,38 @@ class ClarinSpider(scrapy.Spider):
                 continue
             if not url.startswith('http'):
                 url = 'https://clarin.com' + url
-            article.css('.publishedDate::text')
-            article.css('.modificatedDate::text')
+
             maybe_img = article.css('figure img')
-            yield {
+            obj = {
                 'title': article.css('h1::text, h2::text').get(),
                 'volanta': article.css('.volanta::text').get(),
                 'section': article.css('.section::text').get(),
                 'url': url,
                 'image': maybe_img.attrib['data-big'] if maybe_img else None,
-                'date':
             }
+
+            yield obj
             request = scrapy.Request(url, callback=self.parse_article, cb_kwargs=dict(url=url))
             yield request
 
     def parse_article(self, response, url):
         html = ''.join(response.xpath('//div[@class="body-nota"]/*').extract())
-        cleaner = Cleaner(allow_tags=['p', 'br', 'b', 'a', 'strong', 'i', 'em'])
-        yield {
+        content = lxml.html.tostring(cleaner.clean_html(lxml.html.fromstring(html))).decode('utf-8')
+
+        modificated_date = response.css('.modificatedDate::text').get().strip()
+        if not modificated_date:
+            raise ValueError('no modificated date')
+        modificated_date_prefix = 'Actualizado al '
+        print(modificated_date)
+        if not modificated_date.startswith(modificated_date_prefix):
+            raise ValueError('modificated date does not start with ' + modificated_date_prefix)
+        modificated_date = modificated_date[len(modificated_date_prefix):]
+        date = datetime.strptime(modificated_date, '%d/%m/%Y %H:%M')
+
+        obj = {
             'url': url,
-            'content': lxml.html.tostring(cleaner.clean_html(lxml.html.fromstring(html))).decode('utf-8'),
+            'content': content,
+            'date': date.isoformat()
         }
+
+        yield obj
