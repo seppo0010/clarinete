78b1df0fdf17b32d8368782328b9420ecd8c0b21
Sebastian Waisbrot <seppo0010@gmail.com>
2021-07-04 09:48:38 -0300

    restapi docker compose

--- restapi/main.py
+++ restapi/main.py
@@ -1,32 +1,41 @@
-import sys
-import sqlite3
-from flask import Flask, jsonify, request
+import os
+import psycopg2
+import psycopg2.extras
+import urllib
+from flask import Flask, jsonify, request, g
 
-if len(sys.argv) != 2:
-    sys.stderr.write(f'usage: {sys.argv[0]} <sqlite database>\n')
-    sys.exit(1)
 
-dbpath = sys.argv[1]
+def get_db():
+    pg_user = os.getenv("POSTGRES_USER")
+    pg_host = 'news-database'
+    pg_db = os.getenv("POSTGRES_DB")
+    with open('/run/secrets/postgres-password', 'r') as fp:
+        pg_password = fp.read().strip()
+    dsn = f'postgres://{pg_user}:{urllib.parse.quote(pg_password)}@{pg_host}/{pg_db}'
+
+    if 'db' not in g:
+        g.db = pg_connection = psycopg2.connect(dsn)
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
 app = Flask(__name__)
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+app.teardown_appcontext(close_db)
 
 @app.route("/api/last_updated")
 def last_updated():
-    con = sqlite3.connect(dbpath)
-    cur = con.cursor()
-    cur.execute('''SELECT MAX(time) FROM updated''')
+    con = get_db()
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute('''SELECT MAX(time) AS time FROM updated''')
     return jsonify(cur.fetchone())
 
 @app.route("/api/news")
 def news_list():
-    con = sqlite3.connect(dbpath)
-    con.row_factory = dict_factory
-    cur = con.cursor()
+    con = get_db()
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute('''
         SELECT url, title, volanta, section.name AS section, date, source.name AS source, sentiment, summary
         FROM news
@@ -38,23 +47,21 @@ def news_list():
 
 @app.route("/api/news/details")
 def news_details():
+    con = get_db()
     url = request.args.get('url')
     if not url:
         return 400, ''
-    con = sqlite3.connect(dbpath)
-    con.row_factory = dict_factory
-    cur = con.cursor()
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute('''
         SELECT url, title, volanta, section.name AS section, date, content, source.name AS source, summary, sentiment
         FROM news
             JOIN section ON news.section_id = section.id
             JOIN source ON news.source_id = source.id
-        WHERE url = ?''', [url])
+        WHERE url = %s''', [url])
     row = cur.fetchone()
     if not url:
         return 404, ''
     return jsonify(row)
-
 
 if __name__ == '__main__':
   app.run('0.0.0.0', debug=True)
