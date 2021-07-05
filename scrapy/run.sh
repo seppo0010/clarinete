6d7f47fa3c7ec68803ac2b6e301d84bc13530946
Sebastian Waisbrot <seppo0010@gmail.com>
2021-07-05 13:47:50 -0300

    less verbose outputs

--- scrapy/run.sh
+++ scrapy/run.sh
@@ -3,7 +3,7 @@ set -Eeux
 
 while true; do
     cd /scrapy
-    scrapy runspider clarinetear/spiders/clarin.py
-    scrapy runspider clarinetear/spiders/lanacion.py
+    scrapy runspider -L WARNING clarinetear/spiders/clarin.py
+    scrapy runspider -L WARNING clarinetear/spiders/lanacion.py
     sleep 900
 done
