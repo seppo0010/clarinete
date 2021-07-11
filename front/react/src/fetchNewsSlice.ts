8e1bd03641f0ffe5c1e9b1c564fe3b3e9f6148c7
Sebastian Waisbrot <seppo0010@gmail.com>
2021-07-11 09:24:33 -0300

    Limit the number of news

--- front/react/src/fetchNewsSlice.ts
+++ front/react/src/fetchNewsSlice.ts
@@ -2,7 +2,5 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 export const fetchNews = createAsyncThunk('news/fetchNews', async () => {
   const req = await fetch('/api/news')
   const res = await req.json()
-  return res
+  return res.slice(0, 200)
 })
-
-
