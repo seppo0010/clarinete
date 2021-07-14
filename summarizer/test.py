91fd8bfdc07993601da79361e69c1f29e3a86945
Sebastian Waisbrot <seppo0010@gmail.com>
2021-07-14 08:53:59 -0300

    test english summarization

--- summarizer/test.py
+++ summarizer/test.py
@@ -13,6 +13,12 @@ def log(message):
     sys.stderr.write(message + '\n')
 
 class TestSummarizer(unittest.TestCase):
+    def setUp(self):
+        self.wait_for_queue()
+        channel = setup_channel()
+        channel.queue_delete(queue=QUEUE_KEY)
+        channel.queue_delete(queue=RESPONSE_KEY)
+
     def wait_for_queue(self):
         for i in range(10):
             log(f'attempt {i} to connect to queue')
@@ -26,14 +32,12 @@ class TestSummarizer(unittest.TestCase):
                 traceback.print_exc()
         raise Exception('wait for queue timed out')
 
-
     @timeout_decorator.timeout(120)
     def test_summary_es(self):
-        url = 'http://fake-url/'
+        url = 'http://fake-url/es'
         title = 'PREAMBULO'
         content = 'Nos los representantes del pueblo de la Nación Argentina, reunidos en Congreso General Constituyente por voluntad y elección de las provincias que la componen, en cumplimiento de pactos preexistentes, con el objeto de constituir la unión nacional, afianzar la justicia, consolidar la paz interior, proveer a la defensa común, promover el bienestar general, y asegurar los beneficios de la libertad, para nosotros, para nuestra posteridad, y para todos los hombres del mundo que quieran habitar en el suelo argentino: invocando la protección de Dios, fuente de toda razón y justicia: ordenamos, decretamos y establecemos esta Constitución, para la Nación Argentina.'
         language = 'es'
-        self.wait_for_queue()
         channel = setup_channel()
         channel.basic_publish(
             exchange='',
@@ -56,4 +60,34 @@ class TestSummarizer(unittest.TestCase):
             obj = json.loads(body.decode('utf-8'))
             self.assertEqual(obj['url'], url)
             self.assertIn('constitución', obj['summary'].lower())
+            break
+
+    @timeout_decorator.timeout(60)
+    def test_summary_en(self):
+        url = 'http://fake-url/en'
+        title = 'Preamble'
+        content = 'We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.'
+        language = 'en'
+        channel = setup_channel()
+        channel.basic_publish(
+            exchange='',
+            routing_key=QUEUE_KEY,
+            body=json.dumps({
+                'url': url,
+                'title': title,
+                'content': content,
+                'language': language,
+            }),
+            properties=pika.BasicProperties(
+                content_type='application/json',
+                delivery_mode=1,
+            )
+        )
+        run_once(channel)
+        channel = setup_channel()
+        channel.queue_declare(queue=RESPONSE_KEY, durable=True)
+        for method_frame, properties, body in channel.consume(RESPONSE_KEY):
+            obj = json.loads(body.decode('utf-8'))
+            self.assertEqual(obj['url'], url)
+            self.assertIn('constitution', obj['summary'].lower())
             break
