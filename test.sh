ef1cefce27a899a183fbf7f3bf3a911a4b66e46a
Sebastian Waisbrot <seppo0010@gmail.com>
2021-07-16 05:37:06 -0300

    test deduplicator kickoff

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 set -Eeux
-time docker-compose build summarizer_test
+time docker-compose build summarizer_test deduplicator_test
 time docker-compose --project-name clarinete-test-summarizer run summarizer_test
+time docker-compose --project-name clarinete-test-deduplicator run deduplicator_test
