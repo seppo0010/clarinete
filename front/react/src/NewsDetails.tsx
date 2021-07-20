91fd51652b43452496bfd657fd2257950c61adb9
Sebastian Waisbrot <seppo0010@gmail.com>
2021-07-20 14:54:03 -0300

    Archive news in detail view

--- front/react/src/NewsDetails.tsx
+++ front/react/src/NewsDetails.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux'
+import { GlobalHotKeys } from "react-hotkeys";
+
 import Container from '@material-ui/core/Container';
 import Grid, { GridSize } from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -8,14 +9,22 @@ import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
 import { fetchSingleNews, selectNews, deduplicatedIndex } from './singleNewsSlice'
 import {
-  useParams
+  useParams,
+  useHistory,
+  Link,
 } from "react-router-dom";
+import { addURL } from './archivedSlice'
 import type { RootState } from './store'
+
+const keyMap = {
+  ARCHIVE: "a",
+}
 
 function NewsDetails() {
   let { url, source } = useParams<{url: string, source?: string}>()
   url = decodeURIComponent(url)
 
+  const history = useHistory()
   const dispatch = useDispatch()
   const maybeNews = useSelector(selectNews)
   const news = (maybeNews.news &&
@@ -40,6 +49,15 @@ function NewsDetails() {
     }
   }
 
+  const handlers = {
+    ARCHIVE: () => {
+      if (news) {
+        dispatch(addURL(news.url))
+        history.push('/')
+      }
+    },
+  };
+
   const newsStatus = useSelector((state: RootState) => state.singleNews.status)
 
   useEffect(() => {
@@ -47,44 +65,48 @@ function NewsDetails() {
       dispatch(fetchSingleNews(url))
     }
   }, [newsStatus, dispatch, url, news])
-  return (<Container maxWidth='sm'>
-    {news ? <div style={{marginTop: 10, lineHeight: 1.2, paddingTop: 80}}>
-      <Grid container>
-        <Grid item xs={6}>
-          <Link to="/">
-            <ArrowLeftIcon />
-            Volver
-          </Link>
-        </Grid>
-        <Grid item xs={6} style={{textAlign: 'right'}}>
-          <a href={news.url} rel="noreferrer" target="_blank">
-            Ver original
-            <OpenInNewIcon />
-          </a>
-        </Grid>
-      </Grid>
-      {canonicalURL && sources.length > 1 && <Grid container style={{marginTop: 20}}>
-        {sources.map((source, index) => (<Grid item key={index} style={{
-          textAlign: 'center',
-        }} xs={({
-          1: 12,
-          2: 6,
-          3: 4,
-          4: 3,
-          5: 2,
-          6: 2,
-        }[sources.length] || 1) as GridSize}>
-          <Link to={`/${encodeURIComponent(canonicalURL)}/${source}`} title={news.summary}>{source}</Link>
-        </Grid>
-        ))}
-      </Grid>}
-      <h1>{news.title}</h1>
-      {news.summary && <Paper elevation={3} style={{padding: 10}}>{news.summary}</Paper>}
-      <div className="content" dangerouslySetInnerHTML={{__html: news.content}}></div>
-    </div> :
-    <div></div>
-    }
-  </Container>);
+  return (
+    <GlobalHotKeys handlers={handlers} keyMap={keyMap} allowChanges={true}>
+      <Container maxWidth='sm'>
+        {news ? <div style={{marginTop: 10, lineHeight: 1.2, paddingTop: 80}}>
+          <Grid container>
+            <Grid item xs={6}>
+              <Link to="/">
+                <ArrowLeftIcon />
+                Volver
+              </Link>
+            </Grid>
+            <Grid item xs={6} style={{textAlign: 'right'}}>
+              <a href={news.url} rel="noreferrer" target="_blank">
+                Ver original
+                <OpenInNewIcon />
+              </a>
+            </Grid>
+          </Grid>
+          {canonicalURL && sources.length > 1 && <Grid container style={{marginTop: 20}}>
+            {sources.map((source, index) => (<Grid item key={index} style={{
+              textAlign: 'center',
+            }} xs={({
+              1: 12,
+              2: 6,
+              3: 4,
+              4: 3,
+              5: 2,
+              6: 2,
+            }[sources.length] || 1) as GridSize}>
+              <Link to={`/${encodeURIComponent(canonicalURL)}/${source}`} title={news.summary}>{source}</Link>
+            </Grid>
+            ))}
+          </Grid>}
+          <h1>{news.title}</h1>
+          {news.summary && <Paper elevation={3} style={{padding: 10}}>{news.summary}</Paper>}
+          <div className="content" dangerouslySetInnerHTML={{__html: news.content}}></div>
+        </div> :
+        <div></div>
+        }
+      </Container>
+    </GlobalHotKeys>
+  );
 }
 
 export default NewsDetails;
