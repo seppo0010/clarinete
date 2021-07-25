a5a9123ec2ed2bd3e4224eb4b02525bcb068e3fe
Sebastian Waisbrot <seppo0010@gmail.com>
2021-07-25 03:03:45 -0300

    Offline mode

--- front/react/src/index.tsx
+++ front/react/src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -15,6 +16,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
